8dc577ffaaff752d71260be918265d52ddf4570d
seokcheon ju <smallfish06@cau.ac.kr>
2019-12-11 15:30:05 +0900

    add initBoard

--- app/src/main/java/com/example/puyo/SinglePlay.java
+++ app/src/main/java/com/example/puyo/SinglePlay.java
@@ -36,24 +36,35 @@ public class SinglePlay extends AppCompatActivity {
         }
     };
 
-
     private int[][] idArray = {
-            {R.id.iv_sp_00, R.id.iv_sp_01, R.id.iv_sp_02, R.id.iv_sp_03, R.id.iv_sp_04, R.id.iv_sp_05, R.id.iv_sp_06, R.id.iv_sp_07},
-            {R.id.iv_sp_08, R.id.iv_sp_09, R.id.iv_sp_10, R.id.iv_sp_11, R.id.iv_sp_12, R.id.iv_sp_13, R.id.iv_sp_14, R.id.iv_sp_15},
-            {R.id.iv_sp_16, R.id.iv_sp_17, R.id.iv_sp_18, R.id.iv_sp_19, R.id.iv_sp_20, R.id.iv_sp_21, R.id.iv_sp_22, R.id.iv_sp_23},
-            {R.id.iv_sp_24, R.id.iv_sp_25, R.id.iv_sp_26, R.id.iv_sp_27, R.id.iv_sp_28, R.id.iv_sp_29, R.id.iv_sp_30, R.id.iv_sp_31},
-            {R.id.iv_sp_32, R.id.iv_sp_33, R.id.iv_sp_34, R.id.iv_sp_35, R.id.iv_sp_36, R.id.iv_sp_37, R.id.iv_sp_38, R.id.iv_sp_39},
-            {R.id.iv_sp_40, R.id.iv_sp_41, R.id.iv_sp_42, R.id.iv_sp_43, R.id.iv_sp_44, R.id.iv_sp_45, R.id.iv_sp_46, R.id.iv_sp_47},
-            {R.id.iv_sp_48, R.id.iv_sp_49, R.id.iv_sp_50, R.id.iv_sp_51, R.id.iv_sp_52, R.id.iv_sp_53, R.id.iv_sp_54, R.id.iv_sp_55},
-            {R.id.iv_sp_56, R.id.iv_sp_57, R.id.iv_sp_58, R.id.iv_sp_59, R.id.iv_sp_60, R.id.iv_sp_61, R.id.iv_sp_62, R.id.iv_sp_63},
-            {R.id.iv_sp_64, R.id.iv_sp_65, R.id.iv_sp_66, R.id.iv_sp_67, R.id.iv_sp_68, R.id.iv_sp_69, R.id.iv_sp_70, R.id.iv_sp_71},
-            {R.id.iv_sp_72, R.id.iv_sp_73, R.id.iv_sp_74, R.id.iv_sp_75, R.id.iv_sp_76, R.id.iv_sp_77, R.id.iv_sp_78, R.id.iv_sp_79},
-            {R.id.iv_sp_80, R.id.iv_sp_81, R.id.iv_sp_82, R.id.iv_sp_83, R.id.iv_sp_84, R.id.iv_sp_85, R.id.iv_sp_86, R.id.iv_sp_87},
-            {R.id.iv_sp_88, R.id.iv_sp_89, R.id.iv_sp_90, R.id.iv_sp_91, R.id.iv_sp_92, R.id.iv_sp_93, R.id.iv_sp_94, R.id.iv_sp_95},
-            {R.id.iv_sp_96, R.id.iv_sp_97, R.id.iv_sp_98, R.id.iv_sp_99, R.id.iv_sp_100, R.id.iv_sp_101, R.id.iv_sp_102, R.id.iv_sp_103},
-            {R.id.iv_sp_104, R.id.iv_sp_105, R.id.iv_sp_106, R.id.iv_sp_107, R.id.iv_sp_108, R.id.iv_sp_109, R.id.iv_sp_110, R.id.iv_sp_111}
-    };
-
+            { R.id.iv_sp_00, R.id.iv_sp_01, R.id.iv_sp_02, R.id.iv_sp_03, R.id.iv_sp_04, R.id.iv_sp_05, R.id.iv_sp_06,
+                    R.id.iv_sp_07 },
+            { R.id.iv_sp_08, R.id.iv_sp_09, R.id.iv_sp_10, R.id.iv_sp_11, R.id.iv_sp_12, R.id.iv_sp_13, R.id.iv_sp_14,
+                    R.id.iv_sp_15 },
+            { R.id.iv_sp_16, R.id.iv_sp_17, R.id.iv_sp_18, R.id.iv_sp_19, R.id.iv_sp_20, R.id.iv_sp_21, R.id.iv_sp_22,
+                    R.id.iv_sp_23 },
+            { R.id.iv_sp_24, R.id.iv_sp_25, R.id.iv_sp_26, R.id.iv_sp_27, R.id.iv_sp_28, R.id.iv_sp_29, R.id.iv_sp_30,
+                    R.id.iv_sp_31 },
+            { R.id.iv_sp_32, R.id.iv_sp_33, R.id.iv_sp_34, R.id.iv_sp_35, R.id.iv_sp_36, R.id.iv_sp_37, R.id.iv_sp_38,
+                    R.id.iv_sp_39 },
+            { R.id.iv_sp_40, R.id.iv_sp_41, R.id.iv_sp_42, R.id.iv_sp_43, R.id.iv_sp_44, R.id.iv_sp_45, R.id.iv_sp_46,
+                    R.id.iv_sp_47 },
+            { R.id.iv_sp_48, R.id.iv_sp_49, R.id.iv_sp_50, R.id.iv_sp_51, R.id.iv_sp_52, R.id.iv_sp_53, R.id.iv_sp_54,
+                    R.id.iv_sp_55 },
+            { R.id.iv_sp_56, R.id.iv_sp_57, R.id.iv_sp_58, R.id.iv_sp_59, R.id.iv_sp_60, R.id.iv_sp_61, R.id.iv_sp_62,
+                    R.id.iv_sp_63 },
+            { R.id.iv_sp_64, R.id.iv_sp_65, R.id.iv_sp_66, R.id.iv_sp_67, R.id.iv_sp_68, R.id.iv_sp_69, R.id.iv_sp_70,
+                    R.id.iv_sp_71 },
+            { R.id.iv_sp_72, R.id.iv_sp_73, R.id.iv_sp_74, R.id.iv_sp_75, R.id.iv_sp_76, R.id.iv_sp_77, R.id.iv_sp_78,
+                    R.id.iv_sp_79 },
+            { R.id.iv_sp_80, R.id.iv_sp_81, R.id.iv_sp_82, R.id.iv_sp_83, R.id.iv_sp_84, R.id.iv_sp_85, R.id.iv_sp_86,
+                    R.id.iv_sp_87 },
+            { R.id.iv_sp_88, R.id.iv_sp_89, R.id.iv_sp_90, R.id.iv_sp_91, R.id.iv_sp_92, R.id.iv_sp_93, R.id.iv_sp_94,
+                    R.id.iv_sp_95 },
+            { R.id.iv_sp_96, R.id.iv_sp_97, R.id.iv_sp_98, R.id.iv_sp_99, R.id.iv_sp_100, R.id.iv_sp_101,
+                    R.id.iv_sp_102, R.id.iv_sp_103 },
+            { R.id.iv_sp_104, R.id.iv_sp_105, R.id.iv_sp_106, R.id.iv_sp_107, R.id.iv_sp_108, R.id.iv_sp_109,
+                    R.id.iv_sp_110, R.id.iv_sp_111 } };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,11 +76,10 @@ public class SinglePlay extends AppCompatActivity {
                 m_board_Image[i][j] = (ImageView) findViewById(idArray[i][j]);
             }
         }
-
+        initBoard();
         timer.schedule(tt, 0, 100);
 
         timer2.schedule(tt2, 0, 100);
-
 
         board = new Board();
         board.gen_puyo();
@@ -101,7 +111,7 @@ public class SinglePlay extends AppCompatActivity {
                                 stagescore = board.clear_board();
                                 if (stagescore != 0) {
                                     temp = counter;
-                                    while (temp + 5  > counter) {
+                                    while (temp + 5 > counter) {
 
                                     }
                                     int multiplier = 1;
@@ -124,7 +134,7 @@ public class SinglePlay extends AppCompatActivity {
                         latterpoint = new Point(curpoint.x, curpoint.y);
                         compare = counter;
                     }
-                    if ((int) (counter / 30) > 10-speed) {
+                    if ((int) (counter / 30) > 10 - speed) {
                         speed--;
                         compare = counter;
                     }
@@ -138,11 +148,10 @@ public class SinglePlay extends AppCompatActivity {
         t.start();
     }
 
-
     private void drawMyBoard() {
 
-        for (int i = 0; i < ROW; i++) {
-            for (int j = 0; j < COL; j++) {
+        for (int i = 1; i < ROW - 1; i++) {
+            for (int j = 1; j < COL - 1; j++) {
                 if (board.getboard()[j][i] == 1) {
                     m_board_Image[i][j].setImageResource(R.drawable.puyo_blue);
                 } else if (board.getboard()[j][i] == 2) {
@@ -157,6 +166,9 @@ public class SinglePlay extends AppCompatActivity {
             }
         }
 
+    }
+
+    private void initBoard() {
         /* wall */
         for (int i = 0; i < COL; i++) {
             m_board_Image[0][i].setImageResource(R.drawable.wall);
@@ -169,27 +181,26 @@ public class SinglePlay extends AppCompatActivity {
         }
     }
 
-
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent msg) {
 
         switch (keyCode) {
-            case KeyEvent.KEYCODE_DPAD_LEFT:
-                board.move_left();
-                drawMyBoard();
-                break;
-            case KeyEvent.KEYCODE_DPAD_RIGHT:
-                board.move_right();
-                drawMyBoard();
-                break;
-            case KeyEvent.KEYCODE_DPAD_UP:
-                board.spin();
-                drawMyBoard();
-                break;
-            case KeyEvent.KEYCODE_DPAD_DOWN:
-                board.move_down();
-                drawMyBoard();
-                break;
+        case KeyEvent.KEYCODE_DPAD_LEFT:
+            board.move_left();
+            drawMyBoard();
+            break;
+        case KeyEvent.KEYCODE_DPAD_RIGHT:
+            board.move_right();
+            drawMyBoard();
+            break;
+        case KeyEvent.KEYCODE_DPAD_UP:
+            board.spin();
+            drawMyBoard();
+            break;
+        case KeyEvent.KEYCODE_DPAD_DOWN:
+            board.move_down();
+            drawMyBoard();
+            break;
         }
 
         return super.onKeyDown(keyCode, msg);
